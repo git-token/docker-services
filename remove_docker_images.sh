475ff8439678e06a631cea7ab68b183323e17a69
Ryan Tate <ryan.michael.tate@gmail.com>
2017-09-08 22:26:10 -0700

    WIP

---
+++ remove_docker_images.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for var in $(docker images | grep none)
+do
+  if [ ${#var} == 12 ];
+  then
+    echo "Deleting Docker Image tagged as 'none'"
+    echo ${var}
+    docker rmi ${var}
+  fi;
+  done
